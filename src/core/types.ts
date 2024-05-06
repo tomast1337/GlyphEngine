d5b8d39b222844e72958341f9562c3a1eb8ff9dd
tomast1337 <vycasnicolas@gmail.com>
2024-05-06 11:12:21 -0300

    Update Render type to use RenderModes

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,6 +1,8 @@
 import { Context, Buffer } from "../modules/types";
 
+export type RenderModes = string;
+
 export type Render = {
-  preferredElementNodeName: "PRE" | "CANVAS" | "DIV";
+  preferredElementNodeName: RenderModes;
   render: (context: Context, buffer: Buffer) => void;
 };
